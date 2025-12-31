8338bd3ad230fd614b76153a69232a894e1788f9
khristos24 <pofunwa@gmail.com>
2025-12-31 22:59:19 +0800

    Simplify db helper to avoid type errors

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,3 @@
 import { sql } from "@vercel/postgres"
 
-export async function query<T = any>(strings: TemplateStringsArray, ...values: any[]) {
-  return sql<T>(strings as any, ...values)
-}
-
 export { sql }
